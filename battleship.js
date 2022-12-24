82c2f10ba9c9df3ef88debff5040df30d0f698a2
PChabowski <p.chabowski1@gmail.com>
2022-12-24 14:20:27 +0100

    Gra W Okręty
    poprawna wersja gry, ze strony 98, książka z serii Rusz głową, Programowanie w JavaScript. Która jak na razie widzę ma wiele wad.

--- battleship.js
+++ battleship.js
@@ -1,1 +1,32 @@
+var loc1 = 3;
+var loc2 = 4;
+var loc3 = 5;
 
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Podaj zakres od 0 do 6");
+
+    if((guess < 0)||(guess > 6)) {
+       alert("Proszę podać prawidłowy numer komórki!");
+    } else {
+        guesses += 1;
+        
+        if((loc1 == guess)||(loc2 == guess)||(loc3 == guess)) {
+            hits += 1;
+            alert("TRAFIONY!")
+
+            if(hits == 3){
+                isSunk = true;
+                alert("Zatopiłeś mój okręt");
+            }
+        } else {
+			alert("PUDŁO!");
+		}
+    }
+}
+alert("statystyki: zatopiłeś okręt w probie: "+ guesses + " Twoj efektywność wynosi: " + (3/guesses) +".");
