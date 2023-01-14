059c1fe39a0661f63eb0b8d7149bf2e65463a33c
PChabowski <p.chabowski1@gmail.com>
2023-01-14 13:10:07 +0100

    Update battleship.js
    #add if "you won't sink the ship if you don't enter the three coordinates of the ship "

--- battleship.js
+++ battleship.js
@@ -1,9 +1,10 @@
-var loc1 = 3;
-var loc2 = 4;
-var loc3 = 5;
+var locRandom = Math.floor(Math.random()* 5);
+var loc1 = locRandom;
+var loc2 = loc1 + 1;
+var loc3 = loc2 + 1;
 
 var guess;
-var hits = 0;
+var oneHit = false, twoHit = false, treeHit = false;
 var guesses = 0;
 
 var isSunk = false;
@@ -14,18 +15,32 @@ while (isSunk == false) {
     if((guess < 0)||(guess > 6)) {
        alert("Proszę podać prawidłowy numer komórki!");
     } else {
-        guesses += 1;
-        
         if((loc1 == guess)||(loc2 == guess)||(loc3 == guess)) {
-            hits += 1;
-            alert("TRAFIONY!")
-
-            if(hits == 3){
+            if(loc1 == guess && !oneHit){
+				oneHit = true;
+				alert("TRAFIONY!");
+				guesses += 1;
+			} else if(loc1 == guess && oneHit){ alert("To miejśce już zostało trafione! spróbuj inne!"); }
+			
+			if(loc2 == guess && !twoHit){
+				twoHit = true;
+				alert("TRAFIONY!");
+				guesses += 1;
+			} else if(loc2 == guess && twoHit){ alert("To miejśce już zostało trafione! spróbuj inne!"); }
+			
+			if(loc3 == guess && !treeHit){
+				treeHit = true;
+				alert("TRAFIONY!");
+				guesses += 1;
+			} else if(loc3 == guess && treeHit){ alert("To miejśce już zostało trafione! spróbuj inne!"); }
+			
+            if(oneHit && twoHit && treeHit){
                 isSunk = true;
                 alert("Zatopiłeś mój okręt");
             }
         } else {
 			alert("PUDŁO!");
+			guesses += 1;
 		}
     }
 }
